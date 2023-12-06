8e544ecf32f0523ed7415fe1b7ed36e2e3b7aaf2
jesseyproctor <jss.proctor@gmail.com>
2023-12-06 17:52:41 -0500

    app overhaul

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,49 +1,17 @@
-'use client'
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
 
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
-import axios from 'axios';
-import "easymde/dist/easymde.min.css";
-import { useForm, Controller } from 'react-hook-form';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
-
-interface IssueForm {
-    title: string;
-    description: string;
-}
+const IssueForm = dynamic(
+  () => import('@/app/issues/_components/IssueForm'),
+  { 
+    ssr: false,
+    loading: () => <IssueFormSkeleton />
+  }
+);
 
 const NewIssuePage = () => {
-    const {register, control, handleSubmit} = useForm<IssueForm>()
-    const router = useRouter();
-    const [error, setError] = useState('')
-
   return (
-    <div className='max-w-xl'>
-        {error && <Callout.Root color='red' className='mb-5'>
-                <Callout.Text>{error}</Callout.Text>
-            </Callout.Root>}
-        <form 
-            className='space-y-3' 
-            onSubmit={handleSubmit(async (data) => {
-                try {
-                    await axios.post('/api/issues', data)
-                    router.push('/issues')
-                } catch (error) {
-                    setError('An unexpected error accured.')
-                }
-            })}>
-                <TextField.Root>
-                    <TextField.Input placeholder='Title' {...register('title')}/>
-                </TextField.Root>
-                <Controller 
-                    name='description'
-                    control={control}
-                    render={({field}) => <SimpleMDE placeholder='Description' {...field}/>}
-                />
-                <Button>Submit</Button>
-        </form>
-    </div>
+    <IssueForm />
   )
 }
 
